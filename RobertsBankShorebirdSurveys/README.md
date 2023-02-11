711e2f5b65b9107dc2979ca46a1dc1bb2e69bd0c
Sarah Popov <sarah.popov@gmail.com>
2023-02-10 17:55:27 -0800

    Split models.Rmd into parameterized child docs

---
+++ RobertsBankShorebirdSurveys/README.md
@@ -0,0 +1,7 @@
+# Raw data files
+
+The data contained herein represent the data in it's most "raw" form (aside from the actual field books). The majority of files have not been edited or manipulated in any way.
+
+The 1991 and 1992 data files were created from data published in Canham et al. (2021).
+
+The 2013 data file represents a special case as it was compiled from two separate data sources. In this year, surveyor Moira Lemon (ML) handed off surveying to Mark Drever (MD). As such there are two sets of surveys for this year (`BPPeeps2013_MD.xlsx` and `BPPeeps2013_ML.xls`). Because ML was the more experienced surveyor at the time, in any cases where both ML and MD surveyed, ML data was chosen. The daily totals of 2013 data here will therefore differ from the daily totals reported in Canham et al. (2021), which instead averaged the ML and MD totals together. Averaging station-level data in this case is not feasible for station-level analysis as birds frequently moved around between stations during multiple surveys. The data files prepared by both ML and MD are included here in case future data users wish to choose one over the other.
---
+++ models/prepare_dat_ns.R
@@ -0,0 +1,40 @@
+# Prepare dat_ns
+# Create north/south dataset (previously referred to as dat3 in initial draft code)
+# Peeps observation data grouped by north/south station
+# This can only be run after binomial_sr.Rmd has been run
+
+# DAT_NS ===========================================================
+dat_ns <- sqldf::sqldf("select year, 
+                        survey_date, 
+                        julian_day, 
+                        min(start_time) as start_time, 
+                        n_s, 
+                        sum(final_count) as final_count, 
+                        sum(predicted_wesa) as predicted_wesa, 
+                        sum(predicted_dunl) as predicted_dunl, 
+                        p_wesa, 
+                        predicted_ratio, 
+                        avg(raptor_count) as raptor_count, 
+                        tide,
+                        elev_min, 
+                        elev_max, 
+                        elev_median, 
+                        elev_mean, 
+                        elev_range, 
+                        flow, 
+                        total_precip, 
+                        mean_temp, 
+                        u, 
+                        v, 
+                        windspd, 
+                        wind_deg 
+                        from dat 
+                        group by survey_date, n_s;") %>%
+  dplyr::mutate(dos = scale(julian_day),
+                log_wesa = log(predicted_wesa + 1),
+                log_dunl = log(predicted_dunl + 1),
+                year_n = as.numeric(year),
+                year_c = scale(year_n)) %>%
+  dplyr::select(year, survey_date, julian_day, dos, start_time, n_s,
+                final_count, predicted_wesa, predicted_dunl, log_wesa, 
+                log_dunl, dplyr::everything())
